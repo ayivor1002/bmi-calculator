4f086c544fe747432c5a68f59e495983b0a8470e
ayivor1002 <mawuli.ayivor@ipnetinstitute.com>
2022-12-13 17:11:46 +0000

    bmi-calulator

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:imc/screens/input_page.dart';
+
+void main() {
+  runApp( const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.black12,
+          scaffoldBackgroundColor: const Color(0xFF0A0E21)),
+      home: const InputPage(),
+    );
+  }
+}
